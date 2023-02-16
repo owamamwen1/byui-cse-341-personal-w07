23fa5a88caf0deda216fce48cb2a1b17ac64e9db
OgunniyiOwamamwen <owamamwen@outlook.com>
2023-02-16 12:34:07 +0100

    proxy

--- server.js
+++ server.js
@@ -17,13 +17,18 @@ const SECT_SRECT = process.env.SECTION_SRECT
 const app = express();
 
 //Express-session
-app.set('trust proxy', 1) 
-app.use(session({
-  secret: `${SECT_SRECT}`, 
-  resave: false, 
-  saveUninitialized: true,
-  cookie: { secure: true }
-}));
+
+var sess = {
+  secret: `${SECT_SRECT}`,
+  cookie: {}
+}
+
+if (app.get('env') === 'production') {
+  app.set('trust proxy', 1) // trust first proxy
+  sess.cookie.secure = true // serve secure cookies
+}
+app.use(session(sess))
+
 app.use(passport.initialize());
 app.use(passport.session());
 
